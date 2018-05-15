a515c8f0830be0d2d9c922ccad4da1bd9da1eb60
Marcos Luiz Mittag Filho <Marquinhoszx_xd@Outlook.com>
2018-05-15 13:47:43 -0300

    Menu principal lista 02

---
+++ Lista02-WFA/Lista02-WFA/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace Lista02_WFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnexercicio01 = new System.Windows.Forms.Button();
+            this.btncexercicio02 = new System.Windows.Forms.Button();
+            this.btnexercicio03 = new System.Windows.Forms.Button();
+            this.btnexercicio04 = new System.Windows.Forms.Button();
+            this.btnexercicio05 = new System.Windows.Forms.Button();
+            this.btnexercicio06 = new System.Windows.Forms.Button();
+            this.btncexercicio07 = new System.Windows.Forms.Button();
+            this.btnexercicio08 = new System.Windows.Forms.Button();
+            this.btnexercicio09 = new System.Windows.Forms.Button();
+            this.btnexercicio10 = new System.Windows.Forms.Button();
+            this.btncexercicico11 = new System.Windows.Forms.Button();
+            this.btnexercicio12 = new System.Windows.Forms.Button();
+            this.btnexercicio13 = new System.Windows.Forms.Button();
+            this.btnexercicio14 = new System.Windows.Forms.Button();
+            this.btnexercicio15 = new System.Windows.Forms.Button();
+            this.btnexercicio16 = new System.Windows.Forms.Button();
+            this.btnexercicio17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.btnexercicio19 = new System.Windows.Forms.Button();
+            this.btnexercicio20 = new System.Windows.Forms.Button();
+            this.btnexercicio21 = new System.Windows.Forms.Button();
+            this.btnexercicio22 = new System.Windows.Forms.Button();
+            this.btnexercicio23 = new System.Windows.Forms.Button();
+            this.btnexercicio24 = new System.Windows.Forms.Button();
+            this.btnexercicio25 = new System.Windows.Forms.Button();
+            this.btnexercicio26 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnexercicio01
+            // 
+            this.btnexercicio01.Location = new System.Drawing.Point(28, 93);
+            this.btnexercicio01.Name = "btnexercicio01";
+            this.btnexercicio01.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio01.TabIndex = 0;
+            this.btnexercicio01.Text = "Exercicio01";
+            this.btnexercicio01.UseVisualStyleBackColor = true;
+            // 
+            // btncexercicio02
+            // 
+            this.btncexercicio02.Location = new System.Drawing.Point(109, 93);
+            this.btncexercicio02.Name = "btncexercicio02";
+            this.btncexercicio02.Size = new System.Drawing.Size(75, 23);
+            this.btncexercicio02.TabIndex = 1;
+            this.btncexercicio02.Text = "Exercicio02";
+            this.btncexercicio02.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio03
+            // 
+            this.btnexercicio03.Location = new System.Drawing.Point(187, 93);
+            this.btnexercicio03.Name = "btnexercicio03";
+            this.btnexercicio03.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio03.TabIndex = 2;
+            this.btnexercicio03.Text = "Exercicio03";
+            this.btnexercicio03.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio04
+            // 
+            this.btnexercicio04.Location = new System.Drawing.Point(267, 93);
+            this.btnexercicio04.Name = "btnexercicio04";
+            this.btnexercicio04.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio04.TabIndex = 3;
+            this.btnexercicio04.Text = "Exercicio04";
+            this.btnexercicio04.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio05
+            // 
+            this.btnexercicio05.Location = new System.Drawing.Point(71, 122);
+            this.btnexercicio05.Name = "btnexercicio05";
+            this.btnexercicio05.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio05.TabIndex = 4;
+            this.btnexercicio05.Text = "Exercicio05";
+            this.btnexercicio05.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio06
+            // 
+            this.btnexercicio06.Location = new System.Drawing.Point(152, 122);
+            this.btnexercicio06.Name = "btnexercicio06";
+            this.btnexercicio06.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio06.TabIndex = 5;
+            this.btnexercicio06.Text = "Exercicio06";
+            this.btnexercicio06.UseVisualStyleBackColor = true;
+            // 
+            // btncexercicio07
+            // 
+            this.btncexercicio07.Location = new System.Drawing.Point(233, 122);
+            this.btncexercicio07.Name = "btncexercicio07";
+            this.btncexercicio07.Size = new System.Drawing.Size(75, 23);
+            this.btncexercicio07.TabIndex = 6;
+            this.btncexercicio07.Text = "Exercicio07";
+            this.btncexercicio07.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio08
+            // 
+            this.btnexercicio08.Location = new System.Drawing.Point(28, 151);
+            this.btnexercicio08.Name = "btnexercicio08";
+            this.btnexercicio08.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio08.TabIndex = 7;
+            this.btnexercicio08.Text = "Exercicio08";
+            this.btnexercicio08.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio09
+            // 
+            this.btnexercicio09.Location = new System.Drawing.Point(109, 151);
+            this.btnexercicio09.Name = "btnexercicio09";
+            this.btnexercicio09.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio09.TabIndex = 8;
+            this.btnexercicio09.Text = "Exercicio09";
+            this.btnexercicio09.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio10
+            // 
+            this.btnexercicio10.Location = new System.Drawing.Point(190, 151);
+            this.btnexercicio10.Name = "btnexercicio10";
+            this.btnexercicio10.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio10.TabIndex = 9;
+            this.btnexercicio10.Text = "Exercicio10";
+            this.btnexercicio10.UseVisualStyleBackColor = true;
+            // 
+            // btncexercicico11
+            // 
+            this.btncexercicico11.Location = new System.Drawing.Point(267, 151);
+            this.btncexercicico11.Name = "btncexercicico11";
+            this.btncexercicico11.Size = new System.Drawing.Size(75, 23);
+            this.btncexercicico11.TabIndex = 10;
+            this.btncexercicico11.Text = "Exercicio11";
+            this.btncexercicico11.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio12
+            // 
+            this.btnexercicio12.Location = new System.Drawing.Point(71, 180);
+            this.btnexercicio12.Name = "btnexercicio12";
+            this.btnexercicio12.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio12.TabIndex = 11;
+            this.btnexercicio12.Text = "Exercicio12";
+            this.btnexercicio12.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio13
+            // 
+            this.btnexercicio13.Location = new System.Drawing.Point(152, 180);
+            this.btnexercicio13.Name = "btnexercicio13";
+            this.btnexercicio13.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio13.TabIndex = 12;
+            this.btnexercicio13.Text = "Exercicio13";
+            this.btnexercicio13.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio14
+            // 
+            this.btnexercicio14.Location = new System.Drawing.Point(233, 180);
+            this.btnexercicio14.Name = "btnexercicio14";
+            this.btnexercicio14.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio14.TabIndex = 13;
+            this.btnexercicio14.Text = "Exercicio14";
+            this.btnexercicio14.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio15
+            // 
+            this.btnexercicio15.Location = new System.Drawing.Point(37, 209);
+            this.btnexercicio15.Name = "btnexercicio15";
+            this.btnexercicio15.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio15.TabIndex = 14;
+            this.btnexercicio15.Text = "Exercicio15";
+            this.btnexercicio15.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio16
+            // 
+            this.btnexercicio16.Location = new System.Drawing.Point(118, 209);
+            this.btnexercicio16.Name = "btnexercicio16";
+            this.btnexercicio16.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio16.TabIndex = 15;
+            this.btnexercicio16.Text = "Exercicio16";
+            this.btnexercicio16.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio17
+            // 
+            this.btnexercicio17.Location = new System.Drawing.Point(199, 209);
+            this.btnexercicio17.Name = "btnexercicio17";
+            this.btnexercicio17.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio17.TabIndex = 16;
+            this.btnexercicio17.Text = "Exercicio17";
+            this.btnexercicio17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(280, 209);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(75, 23);
+            this.button18.TabIndex = 17;
+            this.button18.Text = "Exercicio18";
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio19
+            // 
+            this.btnexercicio19.Location = new System.Drawing.Point(71, 238);
+            this.btnexercicio19.Name = "btnexercicio19";
+            this.btnexercicio19.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio19.TabIndex = 18;
+            this.btnexercicio19.Text = "Exercicio19";
+            this.btnexercicio19.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio20
+            // 
+            this.btnexercicio20.Location = new System.Drawing.Point(152, 238);
+            this.btnexercicio20.Name = "btnexercicio20";
+            this.btnexercicio20.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio20.TabIndex = 19;
+            this.btnexercicio20.Text = "Exercicio20";
+            this.btnexercicio20.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio21
+            // 
+            this.btnexercicio21.Location = new System.Drawing.Point(233, 238);
+            this.btnexercicio21.Name = "btnexercicio21";
+            this.btnexercicio21.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio21.TabIndex = 20;
+            this.btnexercicio21.Text = "Exercicio21";
+            this.btnexercicio21.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio22
+            // 
+            this.btnexercicio22.Location = new System.Drawing.Point(37, 267);
+            this.btnexercicio22.Name = "btnexercicio22";
+            this.btnexercicio22.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio22.TabIndex = 21;
+            this.btnexercicio22.Text = "Exercicio22";
+            this.btnexercicio22.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio23
+            // 
+            this.btnexercicio23.Location = new System.Drawing.Point(118, 267);
+            this.btnexercicio23.Name = "btnexercicio23";
+            this.btnexercicio23.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio23.TabIndex = 22;
+            this.btnexercicio23.Text = "Exercicio23";
+            this.btnexercicio23.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio24
+            // 
+            this.btnexercicio24.Location = new System.Drawing.Point(199, 267);
+            this.btnexercicio24.Name = "btnexercicio24";
+            this.btnexercicio24.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio24.TabIndex = 23;
+            this.btnexercicio24.Text = "Exercicio24";
+            this.btnexercicio24.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio25
+            // 
+            this.btnexercicio25.Location = new System.Drawing.Point(280, 267);
+            this.btnexercicio25.Name = "btnexercicio25";
+            this.btnexercicio25.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio25.TabIndex = 24;
+            this.btnexercicio25.Text = "Exercicio25";
+            this.btnexercicio25.UseVisualStyleBackColor = true;
+            // 
+            // btnexercicio26
+            // 
+            this.btnexercicio26.Location = new System.Drawing.Point(152, 296);
+            this.btnexercicio26.Name = "btnexercicio26";
+            this.btnexercicio26.Size = new System.Drawing.Size(75, 23);
+            this.btnexercicio26.TabIndex = 25;
+            this.btnexercicio26.Text = "Exercicio26";
+            this.btnexercicio26.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 37);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Menu Princpal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 394);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnexercicio26);
+            this.Controls.Add(this.btnexercicio25);
+            this.Controls.Add(this.btnexercicio24);
+            this.Controls.Add(this.btnexercicio23);
+            this.Controls.Add(this.btnexercicio22);
+            this.Controls.Add(this.btnexercicio21);
+            this.Controls.Add(this.btnexercicio20);
+            this.Controls.Add(this.btnexercicio19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.btnexercicio17);
+            this.Controls.Add(this.btnexercicio16);
+            this.Controls.Add(this.btnexercicio15);
+            this.Controls.Add(this.btnexercicio14);
+            this.Controls.Add(this.btnexercicio13);
+            this.Controls.Add(this.btnexercicio12);
+            this.Controls.Add(this.btncexercicico11);
+            this.Controls.Add(this.btnexercicio10);
+            this.Controls.Add(this.btnexercicio09);
+            this.Controls.Add(this.btnexercicio08);
+            this.Controls.Add(this.btncexercicio07);
+            this.Controls.Add(this.btnexercicio06);
+            this.Controls.Add(this.btnexercicio05);
+            this.Controls.Add(this.btnexercicio04);
+            this.Controls.Add(this.btnexercicio03);
+            this.Controls.Add(this.btncexercicio02);
+            this.Controls.Add(this.btnexercicio01);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnexercicio01;
+        private System.Windows.Forms.Button btncexercicio02;
+        private System.Windows.Forms.Button btnexercicio03;
+        private System.Windows.Forms.Button btnexercicio04;
+        private System.Windows.Forms.Button btnexercicio05;
+        private System.Windows.Forms.Button btnexercicio06;
+        private System.Windows.Forms.Button btncexercicio07;
+        private System.Windows.Forms.Button btnexercicio08;
+        private System.Windows.Forms.Button btnexercicio09;
+        private System.Windows.Forms.Button btnexercicio10;
+        private System.Windows.Forms.Button btncexercicico11;
+        private System.Windows.Forms.Button btnexercicio12;
+        private System.Windows.Forms.Button btnexercicio13;
+        private System.Windows.Forms.Button btnexercicio14;
+        private System.Windows.Forms.Button btnexercicio15;
+        private System.Windows.Forms.Button btnexercicio16;
+        private System.Windows.Forms.Button btnexercicio17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button btnexercicio19;
+        private System.Windows.Forms.Button btnexercicio20;
+        private System.Windows.Forms.Button btnexercicio21;
+        private System.Windows.Forms.Button btnexercicio22;
+        private System.Windows.Forms.Button btnexercicio23;
+        private System.Windows.Forms.Button btnexercicio24;
+        private System.Windows.Forms.Button btnexercicio25;
+        private System.Windows.Forms.Button btnexercicio26;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
